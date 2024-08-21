f056b855c3242437826771f7c5195d4d7fdf63d5
neggan16JT <tobarocampojuanmanuel4@gmail.com>
2024-08-20 19:48:32 -0500

    creacion de nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardProductComponent
   ],
   imports: [
     BrowserModule,
